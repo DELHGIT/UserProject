8d63abd4d534fe150e7dca1a8d29a956188e34c8
ED <elhachemi.debih@gmail.com>
2019-02-27 15:22:11 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {AddUserComponent} from "./add-user/add-user.component";
+import {ListUserComponent} from "./list-user/list-user.component";
+import {EditUserComponent} from "./edit-user/edit-user.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users/login', component: LoginComponent },
+  { path: 'users/add-user', component: AddUserComponent },
+  { path: 'users/list-user', component: ListUserComponent },
+  { path: 'users/edit-user', component: EditUserComponent },
+  {path : '', component : LoginComponent}
+];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const routing = RouterModule.forRoot(routes);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import {routing} from "./app-routing.module";
+import {AuthenticationService} from "./service/auth.service";
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import { AddUserComponent } from './add-user/add-user.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
+import {ListUserComponent} from "./list-user/list-user.component";
+import {UserService} from "./service/user.service";
+import { FilterUserComponent } from './filter-user/filter-user.component';
+import {ProgressBarModule} from "angular-progress-bar"
+import { CustomeCurrencyPipe } from './common/pipes/currencyPipe.pipe';
+import { CurrencyPipe, UpperCasePipe } from '@angular/common';
+import { EmailPipePipe } from './common/pipes/emailPipe.pipe';
+import { HighlightDirective } from './common/directives/Highlight.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [AppComponent,
+      LoginComponent,
+      ListUserComponent,
+      AddUserComponent,
+      EditUserComponent, 
+      FilterUserComponent,
+      CustomeCurrencyPipe ,
+      EmailPipePipe,HighlightDirective],
+   imports: [ BrowserModule,
+       routing, 
+       ReactiveFormsModule,
+       HttpClientModule, 
+       FormsModule, 
+       ProgressBarModule],
+   exports:    [ CustomeCurrencyPipe ],
+   providers: [AuthenticationService, UserService,CustomeCurrencyPipe,CurrencyPipe, EmailPipePipe,UpperCasePipe],
+   bootstrap: [AppComponent]
 })
 export class AppModule { }
